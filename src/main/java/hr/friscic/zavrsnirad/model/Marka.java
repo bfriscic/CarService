9ea734bd3184dba1a5858244f88cc83bee4daab3
Bernard Friščić <bernard.friscic@gmail.com>
2020-09-03 19:35:14 +0200

    Kreiranje klase Entitet ,Klijent ,Marka i Vrsta te mapiranje istih u bazu.

---
+++ src/main/java/hr/friscic/zavrsnirad/model/Marka.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hr.friscic.zavrsnirad.model;
+
+import javax.persistence.Entity;
+
+/**
+ *Klasa Marka se odnosi na marku vozila poput Audi,Volvo,Man,Ducati itd.
+ * @author K1R4
+ */
+
+@Entity
+public class Marka extends Entitet {
+    
+    private String naziv;
+    private String model;
+
+    public String getNaziv() {
+        return naziv;
+    }
+
+    public void setNaziv(String naziv) {
+        this.naziv = naziv;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+    
+    
+    
+}
